5004e1e6caf9ebda58aa43adef0dcd31948600c1
James Hines <jameshines10@gmail.com>
2018-01-26 21:41:21 -0500

    Just woke up from a nap

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const webpack = require("webpack");
 
 module.exports = {
     context: path.resolve(__dirname, "src"),
@@ -6,5 +7,23 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, "dist"),
         filename: 'bundle.js'
-    }
+    },
+    module: {
+        noParse: /node_modules\/localforage\/dist\/localforage.js/,
+        loaders: [{
+            loader: "babel-loader",
+            query: {
+                presets: ["es2015-loose"]
+            }
+        }]
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            riot: "riot/riot"
+        })
+    ],
+    stats: {
+        colors: true
+    },
+    devtool: 'source-map'
 };
